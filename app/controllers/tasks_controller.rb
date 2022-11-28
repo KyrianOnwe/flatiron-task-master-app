2636893dcded7760442b98945ca2662ee79bb230
Kyrian <dr.kyrian.onwe@gmail.com>
2022-11-28 15:55:26 -0600

    Create routes for all and update Task and User

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,4 +3,24 @@ class TasksController < ApplicationController
         tas = Task.all
         render json: tas
     end
+
+    def create
+         task = Task.create(task_params)
+         render json: task, status: :created
+
+    end
+
+    def update 
+
+    end
+
+    def delete 
+
+    end
+
+    private
+
+    def task_params 
+        params.permit(:title, :due_date, :status, :user_id, :project_id, :complete)
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,19 @@ class UsersController < ApplicationController
         render json: us
     end
 
+    def show 
+        user = User.find_by(id: params[:id])
+        render json: user, include: :
+
+    end
+
+    def create 
+
+    end
+
+    private 
+    def user_params
+        params.permit(:name, :role, :password, :email)
+    end
+
 end
