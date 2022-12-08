88d5b46853991dd7d57eaddb4691e33525404722
Kyrian <dr.kyrian.onwe@gmail.com>
2022-12-07 19:39:25 -0600

    Add admin column to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_30_012738) do
+ActiveRecord::Schema.define(version: 2022_12_08_004552) do
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2022_11_30_012738) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.boolean "admin"
   end
 
 end
